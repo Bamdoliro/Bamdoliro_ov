286875b28fc6f84ff85a70115a120eaed48593ac
anys34 <anyeseong34@gmail.com>
2023-10-05 19:28:45 +0900

    initial commit

---
+++ src/main/java/com/bamdoliro/teampage/TeamPageApplication.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.teampage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bamdoliro/teampage/TeampageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.teampage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeampageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
