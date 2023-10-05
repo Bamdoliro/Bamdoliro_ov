c3342eb9c192d110f69f3194a88da7e596c68751
anys34 <anyeseong34@gmail.com>
2023-10-06 00:32:26 +0900

    ADD :: Wind Table
    - Wind 테이블 생성
        - 컬럼(id, wind, createDate)

---
+++ src/main/java/com/bamdoliro/teampage/domain/wind/Wind.java
@@ -0,0 +1,19 @@
+package com.bamdoliro.teampage.domain.wind;
+
+import com.bamdoliro.teampage.domain.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Wind extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String wind;
+}
---
+++ src/main/java/com/bamdoliro/teampage/domain/wind/WindRepository.java
@@ -0,0 +1,6 @@
+package com.bamdoliro.teampage.domain.wind;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WindRepository extends JpaRepository<Wind, Long> {
+}
