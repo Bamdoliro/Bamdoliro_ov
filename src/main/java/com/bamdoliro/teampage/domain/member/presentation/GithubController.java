145b12135dcf62d67e936df780fda637bbb7b310
anys34 <anyeseong34@gmail.com>
2023-11-23 15:18:16 +0900

    ADD :: github update url

--- src/main/java/com/bamdoliro/teampage/domain/member/presentation/GithubController.java
+++ src/main/java/com/bamdoliro/teampage/domain/member/presentation/GithubController.java
@@ -4,13 +4,18 @@ import com.bamdoliro.teampage.domain.member.service.MemberSaveService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Component
+@RequestMapping("/github")
+@RestController
 public class GithubController {
     private final MemberSaveService githubService;
 
-    @Scheduled(cron = "0 0 0 * * ?")
+    @GetMapping("/update")
+    @Scheduled(cron = "0 0 12 * * ?")
     public void membersList() {
         githubService.batchSave();
     }
